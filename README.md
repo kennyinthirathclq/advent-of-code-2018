3b515c939c1f8c56ed53a8ab0876abbfd5a7c14d
Kenny Inthirath <kenny.inthirath@cancerlinq.org>
2018-12-11 00:37:24 -0800

    Uh

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2018
+
+Crude, mostly naive bruteforce attempts at problems within the shortest time that capabilities limit
+
+[Advent of Code 2018](https://adventofcode.com/2018)
--- src/day-4/test.js
+++ src/day-4/test.js
@@ -4,10 +4,14 @@ const { setOne, setTwo } = require('./index')
 describe('Day 4 - Set One', () => {
   it('should do something', () => {
     const rv = setOne(input)
+    expect(rv).toBe(48680)
   })
 
 })
 
 describe('Day 4 - Set Two', () => {
-
+  it('should do something', () => {
+    const rv = setTwo(input)
+    expect(rv).toBe(94826)
+  })
 })
